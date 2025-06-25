c8cb373283d97ad15c5497ca4252a685b2c36b62
mame77 <morinaga.kota@icloud.com>
2025-06-25 19:57:38 +0900

    投稿一覧機能の追加 #8

--- backend/app/api/routers/__init__.py
+++ backend/app/api/routers/__init__.py
@@ -2,8 +2,10 @@
 from fastapi import APIRouter
 from app.api.routers.posts import router as posts_router
 from app.api.routers.auth import router as auth_router
+from app.api.routers.matching import router as matching_router
 
 #routerの登録
 router = APIRouter()
 router.include_router(posts_router, prefix="/posts")
 router.include_router(auth_router, prefix="/auth")
+router.include_router(matching_router, prefix="/api")
